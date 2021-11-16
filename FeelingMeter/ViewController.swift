91f9f1940f4bcf4f285adbe9299ed360e89bcd8e
Camile <camileancines@dextra-sw.com>
2021-11-16 14:29:21 -0300

    Initial Commit

---
+++ FeelingMeter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FeelingMeter
+//
+//  Created by Camile Ancines on 16/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
